2b4a289ba03dc7f67882c91f080a55e7a5e16b6a
OyeLurker <oyelurker@gmail.com>
2025-09-22 01:48:31 +0530

    Prepare for Firebase Functions deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,5 @@ import * as functions from "firebase-functions";
 dotenv.config();
 import { app } from "./app";
 
-const isFirebaseEnv =
-  !!process.env.FUNCTIONS_EMULATOR ||
-  // !!process.env.K_SERVICE ||
-  !!process.env.FIREBASE_CONFIG;
-
-if (isFirebaseEnv) {
-  // Cloud Functions/Emulator
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  // const functions = require("firebase-functions");
-  exports.api = functions.https.onRequest(app);
-} else {
-  // Local development and Render.com
-  const PORT = process.env.PORT || 8080;
-  app.listen(PORT, () => {
-    console.log(`✅ Server running on http://localhost:${PORT}`);
-  });
-}
+// Export your Express app as a Firebase Function
+exports.api = functions.https.onRequest(app);
